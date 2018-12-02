733c134c067b5b0e2e84a483875029c0d87e06c5
ezzaffa88 <so1fien@hotmail.com>
2018-12-02 15:50:23 +0200

    first commit

---
+++ src/agents/AgentA.java
@@ -0,0 +1,15 @@
+package agents;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+
+
+public class AgentA extends Agent {
+  public void setup(){
+     // ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+     // message.addReceiver(new AID("AgentReceiver", AID.ISLOCALNAME));
+     // message.setContent("Hello The World");
+      //send(message);
+  }
+}
---
+++ src/agents/AgentB.java
@@ -0,0 +1,14 @@
+
+package agents;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+
+
+public class AgentB extends Agent {
+    public void setup(){
+     // ACLMessage msg = null;
+     // msg = blockingReceive();
+      //System.out.println(msg.getContent());
+    } 
+}
+
